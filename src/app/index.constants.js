d079e88eabf48664a64009139bc96c0e47355275
Claudio Castro <yayolius@gmail.com>
2016-02-28 22:34:24 -0300

    adding dist to repository in order to make easier the deployment

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -6,7 +6,7 @@
     .module('webapp')
     .constant('malarkey', malarkey)
     .constant('moment', moment)
-    .constant('apiURL','http://localhost:8080');
-    //.constant('apiURL','http://159.203.114.208:3000');
+    //.constant('apiURL','http://localhost:8080');
+    .constant('apiURL','http://159.203.114.208:3000');
   
 })();
